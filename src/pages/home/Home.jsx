86c900a293fb298a991daf85a4aa983ec026c7cb
shadhk <shadhussaink@gmail.com>
2022-03-31 19:53:49 +0530

    react dashboard panel frontend

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,35 @@
+import React from "react"
+import Sidebar from "../../components/sidebar/Sidebar"
+import Navbar from "../../components/navbar/Navbar"
+import "./Home.scss"
+import Widget from "../../components/widgets/Widget"
+import Featured from "../../components/featured/Featured"
+import Chart from "../../components/chart/Chart"
+import List from "../../components/table/Table"
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbar />
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earnings" />
+          <Widget type="balance" />
+        </div>
+        <div className="charts">
+          <Featured />
+          <Chart aspect={2.4 / 1} title="Last 6 months ( Revenue )" />
+        </div>
+        <div className="listContainer">
+          <div className="listTitle">Latest Transactions</div>
+          <List />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import "./Login.scss"
+
+const login = () => {
+  return <div>login</div>
+}
+
+export default login
